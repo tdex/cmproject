25652e6ef9ef4625fb301c87e77c072c2633bcbd
Tarcísio Deschamps Silva <tardchamps94@gmail.com>
2015-05-19 11:52:27 -0300

    ambiente

---
+++ codigo/cmproject/src/com/cmproject/usuarios/Visitante.java
@@ -0,0 +1,49 @@
+package com.cmproject.usuarios;
+
+public class Visitante {
+    private String name;
+    private String telefone;
+    private String email;
+    private String senha;
+    private String endereco;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+//    public void setSenha(String senha) {
+//        this.senha = senha;
+//    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
